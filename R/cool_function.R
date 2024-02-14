b411c2d4907e6295786ec5774bcc9a0b725b22f3
Sebastian Fischer <sebf.fischer@gmail.com>
2024-02-14 10:06:18 +0100

    feat: add a cool function

---
+++ R/cool_function.R
@@ -0,0 +1,7 @@
+#' @title A cool function
+#' @description This function adds 1 to a number.
+#' @param x A number
+#' @export
+cool_function = function(x) {
+  x + 1
+}
---
+++ tests/testthat/test_cool_function.R
@@ -0,0 +1,3 @@
+test_that("cool function works", {
+  expect_equal(cool_function(1), 2)
+})
